d0039e46090ecefac9b1aaf0e30a51e897370563
Nikita Svetlakov <androdn@gmail.com>
2017-03-06 00:41:17 +0500

    Adding class Customer.java

---
+++ src/main/company/Customer.java
@@ -0,0 +1,32 @@
+package main.company;
+
+import java.util.Set;
+
+public class Customer {
+    private String name;
+    private Set projects;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", projects=" + projects +
+                '}';
+    }
+}
